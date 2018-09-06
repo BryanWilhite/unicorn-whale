17daecdb2d3eb0d2a5e75479bf83ab00082476db
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-09-06 10:27:23 -0700

    :tractor: :pencil: added keyframes and callbacks samples, issue #4

--- my-nx-workspace/apps/fire-base-animation/src/app/components/callbacks/callbacks.component.ts
+++ my-nx-workspace/apps/fire-base-animation/src/app/components/callbacks/callbacks.component.ts
@@ -1,12 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition
+} from '@angular/animations';
+
 @Component({
     selector: 'my-nx-workspace-callbacks',
     templateUrl: './callbacks.component.html',
-    styleUrls: ['./callbacks.component.scss']
+    styleUrls: ['./callbacks.component.scss'],
+    animations: [
+        trigger('photoState', [
+            state(
+                'move',
+                style({
+                    transform: 'translateX(-100%)'
+                })
+            ),
+            state(
+                'enlarge',
+                style({
+                    transform: 'scale(1.5)'
+                })
+            ),
+            state(
+                'spin',
+                style({
+                    transform: 'rotateY(180deg) rotateZ(90deg)'
+                })
+            ),
+            transition('* => *', animate('500ms ease'))
+        ])
+    ]
 })
 export class CallbacksComponent implements OnInit {
+    position: string | null;
+
     constructor() {}
 
     ngOnInit() {}
+
+    changePosition(nextState: string | null): void {
+        this.position = nextState;
+    }
+
+    logAnimation($event) {
+        if (!this.position) {
+            console.warn('position is not defined for event:', $event);
+            return;
+        }
+        console.log(`${this.position} animation ${$event.phaseName}`);
+    }
 }
--- my-nx-workspace/apps/fire-base-animation/src/app/components/keyframes/keyframes.component.ts
+++ my-nx-workspace/apps/fire-base-animation/src/app/components/keyframes/keyframes.component.ts
@@ -1,12 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    keyframes,
+    transition
+} from '@angular/animations';
+
 @Component({
     selector: 'my-nx-workspace-keyframes',
     templateUrl: './keyframes.component.html',
-    styleUrls: ['./keyframes.component.scss']
+    styleUrls: ['./keyframes.component.scss'],
+    animations: [
+        trigger('photoState', [
+            state(
+                'move',
+                style({
+                    transform: 'translateX(-100%)'
+                })
+            ),
+            state(
+                'enlarge',
+                style({
+                    transform: 'scale(1.5)'
+                })
+            ),
+            state(
+                'spin',
+                style({
+                    transform: 'rotateY(180deg) rotateZ(90deg)'
+                })
+            ),
+            transition(
+                '* => move',
+                animate(
+                    '2000ms',
+                    keyframes([
+                        style({
+                            transform: 'translateX(0)    rotateY(0)',
+                            offset: 0
+                        }),
+                        style({
+                            transform: 'translateX(50%)  rotateY(90deg)',
+                            offset: 0.33
+                        }),
+                        style({
+                            transform: 'translateY(-75%) rotateY(180deg)',
+                            offset: 0.66
+                        }),
+                        style({ transform: 'translateX(-100%)', offset: 1.0 })
+                    ])
+                )
+            ),
+            transition('* => *', animate('500ms ease'))
+        ])
+    ]
 })
 export class KeyframesComponent implements OnInit {
+    position: string | null;
+
     constructor() {}
 
     ngOnInit() {}
+
+    changePosition(nextState: string | null): void {
+        this.position = nextState;
+    }
 }
