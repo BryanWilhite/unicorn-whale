685d7009720a1deacb3ae648aa1171c80a6b64cb
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-09-05 18:09:41 -0700

    :tractor: added /components folder, issue #4

--- my-nx-workspace/apps/fire-base-animation/src/app/app.module.ts
+++ my-nx-workspace/apps/fire-base-animation/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 
+import { AppComponent } from './components/app.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, NxModule.forRoot()],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
