7de1f00ddcf5200f9b08e61d87a5dd23c782f789
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-09-05 20:53:24 -0700

    :hammer: :pencil: :lipstick: added multi-state and pop-over animations

--- my-nx-workspace/apps/fire-base-animation/src/app/components/multi-state/multi-state.component.ts
+++ my-nx-workspace/apps/fire-base-animation/src/app/components/multi-state/multi-state.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition
+} from '@angular/animations';
+
 @Component({
-  selector: 'my-nx-workspace-multi-state',
-  templateUrl: './multi-state.component.html',
-  styleUrls: ['./multi-state.component.scss']
+    selector: 'my-nx-workspace-multi-state',
+    templateUrl: './multi-state.component.html',
+    styleUrls: ['./multi-state.component.scss'],
+    animations: [
+        trigger('photoState', [
+            state(
+                'move',
+                style({
+                    transform: 'translateX(-100%)'
+                })
+            ),
+            state(
+                'enlarge',
+                style({
+                    transform: 'scale(1.5)'
+                })
+            ),
+            state(
+                'spin',
+                style({
+                    transform: 'rotateY(180deg) rotateZ(90deg)'
+                })
+            ),
+            transition('* => *', animate('500ms ease'))
+        ])
+    ]
 })
 export class MultiStateComponent implements OnInit {
+    position: string | null;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    changePosition(nextState: string | null): void {
+        this.position = nextState;
+    }
 }
--- my-nx-workspace/apps/fire-base-animation/src/app/components/pop-over/pop-over.component.ts
+++ my-nx-workspace/apps/fire-base-animation/src/app/components/pop-over/pop-over.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition
+} from '@angular/animations';
 
 @Component({
-  selector: 'my-nx-workspace-pop-over',
-  templateUrl: './pop-over.component.html',
-  styleUrls: ['./pop-over.component.scss']
+    selector: 'my-nx-workspace-pop-over',
+    templateUrl: './pop-over.component.html',
+    styleUrls: ['./pop-over.component.scss'],
+    animations: [
+        trigger('popOverState', [
+            state(
+                'show',
+                style({
+                    opacity: 1
+                })
+            ),
+            state(
+                'hide',
+                style({
+                    opacity: 0
+                })
+            ),
+            transition('show => hide', animate('600ms ease-out')),
+            transition('hide => show', animate('1000ms ease-in'))
+        ])
+    ]
 })
-export class PopOverComponent implements OnInit {
+export class PopOverComponent {
+    show = false;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    get stateName() {
+        return this.show ? 'show' : 'hide';
+    }
 
+    toggle() {
+        this.show = !this.show;
+    }
 }
