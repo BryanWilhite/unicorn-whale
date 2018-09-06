4eb6cd59a296eec2df9720da95274afbdf597a39
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-09-06 12:34:32 -0700

    :tractor: :pencil: :lipstick: added lists sample, resolves #4

--- my-nx-workspace/apps/fire-base-animation/src/app/components/lists/lists.component.ts
+++ my-nx-workspace/apps/fire-base-animation/src/app/components/lists/lists.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
+    query,
+    stagger
+} from '@angular/animations';
+
 @Component({
     selector: 'my-nx-workspace-lists',
     templateUrl: './lists.component.html',
-    styleUrls: ['./lists.component.scss']
+    styleUrls: ['./lists.component.scss'],
+    animations: [
+        trigger('photosAnimation', [
+            transition('* => *', [
+                query('img', style({ transform: 'translateX(-100%)' })),
+                query(
+                    'img',
+                    stagger('600ms', [
+                        animate('900ms', style({ transform: 'translateX(0)' }))
+                    ])
+                )
+            ])
+        ])
+    ]
 })
 export class ListsComponent implements OnInit {
-    constructor() {}
+    photos: string[];
+
+    constructor() {
+        this.photos = [
+            'https://placekitten.com/121/121',
+            'https://placekitten.com/121/121',
+            'https://placekitten.com/121/121'
+        ];
+    }
 
     ngOnInit() {}
 }
