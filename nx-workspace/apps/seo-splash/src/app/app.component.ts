b240dd31de028109bde46e394e5117b0a5ae7a11
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-04-25 20:12:12 -0700

    :lipstick: :hammer: issue #14

--- nx-workspace/apps/seo-splash/src/app/app.component.ts
+++ nx-workspace/apps/seo-splash/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'nx-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'seo-splash';
+
+  ngAfterViewInit(): void {
+    const main = Array.from(document.getElementsByTagName('main')).find(i => true) as HTMLMainElement;
+    main.classList.add('hidden');
+    setTimeout(() => { main.classList.add('collapsed'); }, 2000);
+  }
 }
